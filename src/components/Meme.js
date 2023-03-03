3ce61a845b8f255608a6d30ea34f37e673f5aa83
GalicaYlber <galica.ylber@gmail.com>
2023-03-04 01:56:29 +0200

    From gone

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,17 @@
 export default function Meme() {
+	function getMemeImage() {
+		console.log("Clicked");
+	}
+
 	return (
 		<div className="meme">
-			<form className="meme-form">
+			<div className="meme-form">
 				<div className="meme-input">
 					<input id="meme-toptext" placeholder="Top Text"></input>
 					<input id="meme-bottext" placeholder="Bottom Text"></input>
 				</div>
-				<input type="submit" className="meme-submit" value="Get a new meme image"></input>
-			</form>
+				<input type="submit" className="meme-submit" onClick={getMemeImage} value="Get a new meme image"></input>
+			</div>
 		</div>
 	)
 }
